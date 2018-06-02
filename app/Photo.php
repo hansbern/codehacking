96ec40021de39de68913946c02bda1b9f188df7c
hansbern <bernad.has@gmail.com>
2018-06-02 11:54:17 -0400

    Users working 100% excepts security

--- app/Photo.php
+++ app/Photo.php
@@ -8,5 +8,18 @@ class Photo extends Model
 {
     //
 
+    protected $uploads ="/images/";
+
     protected $fillable = ['file'];
+
+
+    public function getFileAttribute($photo){
+
+
+        return $this->uploads . $photo;
+
+    }
+
+
+
 }
